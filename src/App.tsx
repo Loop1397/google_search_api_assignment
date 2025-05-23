f6240fef33cee549d030b01fb91eb52b825b3575
Loop1397 <shwlsdn0906@gmail.com>
2025-05-23 22:36:27 +0900

    feat: 검색을 위한 엔터 반응 코드 작성
    input과 onKeyDown을 이용해 input 내에서 Enter키를 입력했을 시 특정 함수가 실행되도록 코드 작성

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [text, setText] = useState<string>("");
+
+  const activeEnter = (e) => {
+    if (e.key === 'Enter') {
+      alert(`${text}!!!`);
+    }
+  }
 
   return (
     <div>
-      <textarea></textarea>
+      <input
+        value={text}
+        onChange={event => {
+          setText(event.target.value);
+        }}
+        onKeyDown={e => activeEnter(e)}
+      ></input>
+      <p>{text}</p>
     </div>
   )
 }
