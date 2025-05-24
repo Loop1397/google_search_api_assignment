67ca04393f285c1683ae69bf0383676c1ae7228f
Loop1397 <shwlsdn0906@gmail.com>
2025-05-24 23:55:52 +0900

    feat: Google Custom Search API 기능 테스트
    - Google Custom Search API를 활용하여 검색할 수 있는 환경 구축 완료
    - 검색 엔진과 api키는 env파일로 관리

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
 import { useState } from 'react'
 import './App.css'
+import axios from 'axios';
 
 function App() {
   const [text, setText] = useState<string>("");
+  const [result, setResult] = useState<any>(null);
 
-  const activeEnter = (e) => {
+  const activeEnter = async (e: any) => {
     if (e.key === 'Enter') {
-      alert(`${text}!!!`);
+      const response: any = await axios.get(`https://www.googleapis.com/customsearch/v1`, {
+        params: {
+          cx: import.meta.env.VITE_CUSTOM_SEARCH_ENGINE_ID,
+          key: import.meta.env.VITE_API_KEY,
+          q: text,
+        },
+      });
+
+      console.log(response.data.items);
+      // setResult(response.data.items[0].title);
     }
   }
 
@@ -19,7 +30,7 @@ function App() {
         }}
         onKeyDown={e => activeEnter(e)}
       ></input>
-      <p>{text}</p>
+      <p>{result}</p>
     </div>
   )
 }
