efbe72302379576b844a237f7e2709e622a295aa
Loop1397 <shwlsdn0906@gmail.com>
2025-05-27 21:40:55 +0900

    feat: header 제작
    검색 입력창을 상단에 고정하는 방식으로 작성

--- src/apis/GoogleCustomSearchAPI.ts
+++ src/apis/GoogleCustomSearchAPI.ts
@@ -7,7 +7,10 @@ type SearchResult = {
     htmlSnippet: string;
     htmlTitle: string;
     kind: string;
+    // target url
     link: string;
+    // pagemap안의 cse_thumbnail가 존재할 경우 썸네일 이미지로 사용 가능
+    // cse_thumbnail은 array로, cse_thumnail[0].src가 썸네일의 이미지 주소
     pagemap: object;
     snippet: string;
     title: string;
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -29,19 +29,36 @@ function App() {
   }
 
   return (
-    <div>
-      <input
-        value={text}
-        onChange={event => {
-          setText(event.target.value);
-        }}
-        onKeyDown={event => {
-          if (event.key === 'Enter') {
-            activeEnter();
-          }
-        }}
-      ></input>
+    <div id="content">
+      <header>
+        <div id="input-box">
+          <input
+            value={text}
+            onChange={event => {
+              setText(event.target.value);
+            }}
+            onKeyDown={event => {
+              if (event.key === 'Enter') {
+                activeEnter();
+              }
+            }}
+          />
+        </div>
+
+      </header>
       <p>{ }</p>
+      <a href="https://x.com/NEKOMANYANKO/status/1921451051959325096">竹猫（ちゃろー！） on X: \"なんで忙しー日はライフ10なの ...</a>
+      <p>May 11, 2025 ... なんで忙しー日はライフ10なの〜 カレイドスコープは50じゃないの〜？</p>
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDkWOjzL1pMnggqccpsEEBHjVvjWqc6Y5thpNSLUXDRIcQ9hjoUQJRebRQ&s" style={{ width: "200px", height: '200px' }} />
+      <a href="https://x.com/NEKOMANYANKO/status/1921451051959325096">竹猫（ちゃろー！） on X: \"なんで忙しー日はライフ10なの ...</a>
+      <p>May 11, 2025 ... なんで忙しー日はライフ10なの〜 カレイドスコープは50じゃないの〜？</p>
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDkWOjzL1pMnggqccpsEEBHjVvjWqc6Y5thpNSLUXDRIcQ9hjoUQJRebRQ&s" style={{ width: "200px", height: '200px' }} />
+      <a href="https://x.com/NEKOMANYANKO/status/1921451051959325096">竹猫（ちゃろー！） on X: \"なんで忙しー日はライフ10なの ...</a>
+      <p>May 11, 2025 ... なんで忙しー日はライフ10なの〜 カレイドスコープは50じゃないの〜？</p>
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDkWOjzL1pMnggqccpsEEBHjVvjWqc6Y5thpNSLUXDRIcQ9hjoUQJRebRQ&s" style={{ width: "200px", height: '200px' }} />
+      <a href="https://x.com/NEKOMANYANKO/status/1921451051959325096">竹猫（ちゃろー！） on X: \"なんで忙しー日はライフ10なの ...</a>
+      <p>May 11, 2025 ... なんで忙しー日はライフ10なの〜 カレイドスコープは50じゃないの〜？</p>
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDkWOjzL1pMnggqccpsEEBHjVvjWqc6Y5thpNSLUXDRIcQ9hjoUQJRebRQ&s" style={{ width: "200px", height: '200px' }} />
     </div>
   )
 }
